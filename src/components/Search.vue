5e98cc3c47784ee1618719cd6f258f450e8cf013
emilioff8822 <emilioff@hotmail.it>
2023-05-03 14:01:40 +0200

    commit-readme-searchvue

---
+++ src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="search-container">
+    <input
+      type="text"
+      v-model="searchText"
+      @input="$emit('search-input', searchText)"
+      placeholder="Cerca carta"
+    />
+    <select v-model="selectedType" @change="$emit('type-selected', selectedType)">
+      <option value="">Seleziona tipo</option>
+      <option v-for="type in cardTypes" :key="type" :value="type">{{ type }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText: "",
+      selectedType: "",
+      cardTypes: [
+        // Aggiungi i tipi di carte qui
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.search-container {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+</style>
